116fe11d0dd52a14c2889a7cdc8ec2a67d3a47c3
SouravsGithub <souravsemail2001@gmail.com>
2022-09-21 16:11:14 +0530

    on going todo app project

---
+++ app.js
@@ -0,0 +1,32 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener("click", (event) => {
+  // Prevent form from submitting
+  event.preventDefault();
+  // Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // Create LI
+  const newTodo = document.createElement("li");
+  newTodo.textContent = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.append(newTodo);
+  // CHECK MARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+  completedButton.classList.add("complete-btn");
+  todoDiv.append(completedButton);
+  // CHECK TRASH BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+  trashButton.classList.add("trash-btn");
+  todoDiv.append(trashButton);
+  // Attach the div to the UL
+  todoList.append(todoDiv);
+  // Clear todo input value
+  todoInput.value = "";
+});
