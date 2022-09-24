bc5c2e2f0d8f01974ff4b62a3a9f0fd65bcde2ab
SouravsGithub <souravsemail2001@gmail.com>
2022-09-24 10:22:19 +0530

    fixed some bugs and refactored the code

--- app.js
+++ app.js
@@ -4,40 +4,45 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 // Event Listeners
-todoButton.addEventListener("click", (event) => {
-  // Prevent form from submitting
-  event.preventDefault();
-  // Todo DIV
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
-  // Create LI
-  const newTodo = document.createElement("li");
-  newTodo.textContent = todoInput.value;
-  newTodo.classList.add("todo-item");
-  todoDiv.append(newTodo);
-  // CHECK MARK BUTTON
-  const completedButton = document.createElement("button");
-  completedButton.innerHTML = `<i class="fas fa-check"></i>`;
-  completedButton.classList.add("complete-btn");
-  todoDiv.append(completedButton);
-  // CHECK TRASH BUTTON
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
-  trashButton.classList.add("trash-btn");
-  todoDiv.append(trashButton);
-  // Attach the div to the UL
-  todoList.append(todoDiv);
-  // Clear todo input value
-  todoInput.value = "";
-});
+todoButton.addEventListener("click", createTodo);
 
 todoList.addEventListener("click", (event) => {
   // Delete Todo
-  if (event.target.classList[0] === "trash-btn") {
+  if (event.target.classList.contains("trash-btn")) {
     event.target.parentElement.remove();
   }
   // Check Mark Todo
-  if (event.target.classList[0] === "complete-btn") {
+  if (event.target.classList.contains("complete-btn")) {
     event.target.parentElement.classList.toggle("completed");
   }
 });
+
+// Functions
+function createTodo(event) {
+  // Prevent form from submitting
+  event.preventDefault();
+  if (todoInput.value.length) {
+    // Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // Create LI
+    const newTodo = document.createElement("li");
+    newTodo.textContent = todoInput.value.trim();
+    newTodo.classList.add("todo-item");
+    todoDiv.append(newTodo);
+    // CHECK MARK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.append(completedButton);
+    // CHECK TRASH BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.append(trashButton);
+    // Attach the div to the UL
+    todoList.append(todoDiv);
+  }
+  // Clear todo input value
+  todoInput.value = "";
+}
